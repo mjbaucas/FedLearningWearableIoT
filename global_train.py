c636aa7a606075cfb38cece7c13ee05ccd582ff0
mjbaucas <mjbaucas@gmail.com>
2022-06-15 17:19:04 -0400

    Minor changes

--- global_train.py
+++ global_train.py
@@ -79,7 +79,7 @@ def evaluate_model(trainX, trainy, testX, testy):
     model.add(Conv1D(filters=64, kernel_size=3, activation='relu', input_shape=(n_timesteps,n_features)))
     model.add(Conv1D(filters=64, kernel_size=3, activation='relu'))
     model.add(Dropout(0.5))
-    model.add(MaxPooling1D(pool_size=2))
+    model.add(MaxPool1D(pool_size=2))
     model.add(Flatten())
     model.add(Dense(100, activation='relu'))
     model.add(Dense(n_outputs, activation='softmax'))

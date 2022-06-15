5db52b154e0e76f3c160a9d006bdeb3a05325e64
mjbaucas <mjbaucas@gmail.com>
2022-06-15 17:12:17 -0400

    Changed parameters

--- global_train.py
+++ global_train.py
@@ -25,7 +25,7 @@ def aggregate_weights(weights, models):
 
 # fit and evaluate a model
 def evaluate_model(trainX, trainy, testX, testy):
-	verbose, epochs, batch_size = 0, 10, 32
+	verbose, epochs, batch_size = 0, 5, 2
 	n_timesteps, n_features, n_outputs = trainX.shape[1], trainX.shape[2], trainy.shape[1]
 	model = Sequential()
 	model.add(Conv1D(filters=64, kernel_size=3, activation='relu', input_shape=(n_timesteps,n_features)))
